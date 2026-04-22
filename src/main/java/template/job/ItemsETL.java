9494f2925f3cc8b0fe99d040086977128c0f409a
Kamil Mazurek <42776173+kamilmazurek@users.noreply.github.com>
2026-04-22 22:14:10 +0200

    Replace JdbcInputFormat with Table API

--- src/main/java/template/job/ItemsETL.java
+++ src/main/java/template/job/ItemsETL.java
@@ -1,33 +1,44 @@
 package template.job;
 
-import org.apache.flink.api.java.typeutils.RowTypeInfo;
-import org.apache.flink.connector.jdbc.JdbcInputFormat;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import template.mapper.ItemMapper;
 
-import static org.apache.flink.api.common.typeinfo.Types.STRING;
+import static org.apache.flink.api.common.RuntimeExecutionMode.BATCH;
 
 public class ItemsETL {
 
     public static void main(String[] args) throws Exception {
-        var env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
-
         var dbUrl = System.getenv("DB_URL");
         var dbUser = System.getenv("DB_USER");
         var dbPassword = System.getenv("DB_PASSWORD");
 
-        var jdbcInputFormat = JdbcInputFormat.buildJdbcInputFormat()
-                .setDBUrl(dbUrl)
-                .setUsername(dbUser)
-                .setPassword(dbPassword)
-                .setDrivername("org.postgresql.Driver")
-                .setQuery("SELECT id, name, description FROM items")
-                .setRowTypeInfo(new RowTypeInfo(STRING, STRING, STRING))
-                .finish();
+        var env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setRuntimeMode(BATCH);
+        var tableEnv = StreamTableEnvironment.create(env);
 
-        var items = env.createInput(jdbcInputFormat);
-        items.print();
-        env.execute("ItemsETL");
+        tableEnv.executeSql(String.format("""
+                    CREATE TABLE items (
+                        id STRING,
+                        name STRING,
+                        description STRING
+                    ) WITH (
+                        'connector' = 'jdbc',
+                        'url' = '%s',
+                        'table-name' = 'items',
+                        'driver' = 'org.postgresql.Driver',
+                        'username' = '%s',
+                        'password' = '%s'
+                    )
+                """, dbUrl, dbUser, dbPassword));
+
+        var resultTable = tableEnv.sqlQuery("SELECT * FROM items");
+
+        tableEnv.toDataStream(resultTable)
+                .map(new ItemMapper())
+                .print();
+
+        env.execute();
     }
 
 }
---
+++ src/main/java/template/model/Item.java
@@ -0,0 +1,16 @@
+package template.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Item {
+
+    private String id;
+
+    private String name;
+
+    private String description;
+
+}
