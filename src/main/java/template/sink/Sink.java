7c85ee9f0404fcb1bfb1661818051972d4f6431e
Kamil Mazurek <42776173+kamilmazurek@users.noreply.github.com>
2026-05-18 22:27:16 +0200

    Use classes from Apache Flink packages

--- src/main/java/template/sink/Sink.java
+++ src/main/java/template/sink/Sink.java
@@ -1,17 +1,20 @@
 package template.sink;
 
-import com.mongodb.client.model.ReplaceOneModel;
-import com.mongodb.client.model.ReplaceOptions;
+
 import lombok.SneakyThrows;
 import org.apache.flink.connector.mongodb.sink.MongoSink;
+import org.apache.flink.mongodb.shaded.com.mongodb.client.model.ReplaceOneModel;
+import org.apache.flink.mongodb.shaded.com.mongodb.client.model.ReplaceOptions;
+import org.apache.flink.mongodb.shaded.org.bson.BsonDocument;
+import org.apache.flink.mongodb.shaded.org.bson.Document;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.bson.BsonDocument;
-import org.bson.Document;
+
 import template.connection.MongodbConnectionParameters;
 import template.model.Item;
 
-import static com.mongodb.client.model.Filters.eq;
+import static org.apache.flink.mongodb.shaded.com.mongodb.client.model.Filters.eq;
+
 
 public class Sink {
 
