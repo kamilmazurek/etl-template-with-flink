fcdecb22436cc20092bd51982b4b547acbc71d9b
Kamil Mazurek <42776173+kamilmazurek@users.noreply.github.com>
2026-06-13 23:16:54 +0200

    Remove data files, as test resources already provide test data

--- data/02_items.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO items (id, name, description)
-VALUES ('item-a', 'Item A', 'Test item A'),
-       ('item-b', 'Item B', 'Test item B'),
-       ('item-c', 'Item C', 'Test item C'),
-       ('item-d', 'Item D', 'Test item D');
